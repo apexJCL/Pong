b0e351edac5b667935cf9e7a2bade5709d9b5621
José Carlos <12030800@itcelaya.edu.mx>
2016-08-04 11:46:58 -0500

    movement restriction

--- paleta.lua
+++ paleta.lua
@@ -1,6 +1,17 @@
 Paleta = {}
+--[[
 
-function Paleta:nuevo(imagen, px, teclaUp, teclaDown)
+Crea una nueva paleta
+Recibe:
++ Imagen : Sprite a dibujar
++ px : Posición en X
++ teclaUp : Tecla con la cual la paleta irá hacia arriba
++ teclaDown: Tecla con la cual irá hacía abajo
++ limSup y limInf : Establece limites para ir hacia arriba o abajo, sino se
+  - especifica con el tamaño de la pantalla
+
+]]
+function Paleta:nuevo(imagen, px, teclaUp, teclaDown, limSup, limInf)
   -- Local porque sólo se usará aquí, no en otro lado
   local w, h = imagen:getDimensions()
   propiedades = {
@@ -8,13 +19,15 @@ function Paleta:nuevo(imagen, px, teclaUp, teclaDown)
     y = 0,
     ancho = w,
     alto = h,
-    velocidad = 150,
+    velocidad = 200,
     -- Tecla para moverse hacía arriba
     tUp = teclaUp,
     -- Tecla para moverse hacía abajo
     tDown = teclaDown,
     -- El sprite de la paleta
-    drawable = imagen
+    drawable = imagen,
+    minY = limSup or h/2,
+    maxY = love.graphics.getHeight() - h/2
   }
   self.__index = self
   return setmetatable(propiedades, self)
@@ -26,11 +39,11 @@ end
 
 function Paleta:actualizar(dt)
 
-  if love.keyboard.isDown (self.tDown) then
+  if love.keyboard.isDown (self.tDown) and (self.y ) <= self.maxY then
     self.y = self.y + (self.velocidad * dt)
   end
 
-  if love.keyboard.isDown (self.tUp) then
+  if love.keyboard.isDown (self.tUp) and self.y >= self.minY then
     self.y = self.y - (self.velocidad * dt)
   end
 
